4690695077baa8dbbdb142af098e3c7eb4134c59
Erick Nava <erick.nava@fastcode.today>
2025-05-27 15:30:15 -0600

    Updates swagger url

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('trust proxy', true); // 👈 Add this line
+
 
 // Middleware
 app.use(cors());               // Optional: allows cross-origin API testing
